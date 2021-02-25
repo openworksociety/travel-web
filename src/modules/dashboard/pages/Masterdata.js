f984e8b062b48d2b3c70306c76148b59efb66745
hkanure <hkanure@mediaocean.com>
2021-02-25 23:53:11 +0530

    added new packaging structure

---
+++ src/modules/dashboard/pages/Masterdata.js
@@ -0,0 +1,12 @@
+import { Typography } from "@material-ui/core";
+import React from "react";
+
+function Masterdata() {
+  return (
+    <div>
+      <Typography variant="h2">This is Masterdata page</Typography>
+    </div>
+  );
+}
+
+export default Masterdata;
