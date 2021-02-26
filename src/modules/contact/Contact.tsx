afd91b28141857b1cf1ae5016e72746048df7d35
Sukumaar <sukumaar.neo@gmail.com>
2021-02-26 00:32:33 -0500

    contact.tsx updated

--- src/modules/contact/Contact.tsx
+++ src/modules/contact/Contact.tsx
@@ -1,24 +1,22 @@
 import React from "react";
 import { Box, Typography } from "@material-ui/core";
-import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import { makeStyles, Theme } from '@material-ui/core/styles';
+//import createStyles from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
 
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    root: {
-      '& > *': {
-        margin: theme.spacing(1),
-        width: '25ch',
-      },
-    },
-  })
-);
+// const useStyles = makeStyles((theme: Theme) =>
+//   createStyles({
+//     root: {
+//       '& > *': {
+//         margin: theme.spacing(1),
+//         width: '25ch',
+//       },
+//     },
+//   })
+// );
 
 const useStylesCard = makeStyles({
   root: {
